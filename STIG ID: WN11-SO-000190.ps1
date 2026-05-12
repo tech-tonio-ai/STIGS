5d9f5f08fdc9aa855d594a608f48141bd4619c90
Antonio Isaacs <ajvi.usa.2024@gmail.com>
2026-05-12 10:27:44 -0400

    Update STIG ID: WN11-SO-000190.ps1

--- STIG ID: WN11-SO-000190.ps1
+++ STIG ID: WN11-SO-000190.ps1
@@ -1,3 +1,39 @@
+<#
+.SYNOPSIS
+    This PowerShell script that checks if the following registry value does not exist or is not configured as specified  creates and validates it:
+
+Registry Hive: HKEY_LOCAL_MACHINE
+Registry Path: \SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters\
+
+Value Name: SupportedEncryptionTypes
+
+Value Type: REG_DWORD
+Value: 0x7ffffff8 (2147483640)
+
+#-------
+.NOTES
+    Author          : Antonio Isaacs
+    LinkedIn        : 
+    GitHub          : github.com/tech-tonio-ai
+    Date Created    : 2026-12-05
+    Last Modified   : 2026-12-05
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000190
+    Documentation   : https://stigaview.com/products/win11/v2r3/WN11-SO-000190/
+
+.TESTED ON
+    Date(s) Tested  : 2026-12-05
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+Save and Run it as:
+    WN11-SO-000190.ps1 
+#>
+
 # WN11-SO-000190 Remediation
 # Configure secure Kerberos encryption types
 
